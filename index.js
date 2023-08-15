1141b192f6da7f8b38ff45cd7aa7d89be7af19dc
Quaid Ahmed <inoxent5050@gmail.com>
2023-08-15 22:11:40 +0500

    expo push notification rest api

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('rest api response from  Quaid Ahmed!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const { Expo } = require('expo-server-sdk');
+
+const app = express();
+const expo = new Expo();
+
+// Endpoint to send push notifications
+app.post('/send-notification', async (req, res) => {
+    const { tokens, title, message } = req.body;
+
+    // Create an array of push notification messages
+    const messages = tokens.map(token => ({
+        to: token,
+        sound: 'default',
+        title,
+        body: message,
+    }));
+
+    try {
+        // Send the push notifications
+        const chunks = expo.chunkPushNotifications(messages);
+        const tickets = [];
+        for (const chunk of chunks) {
+            const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            tickets.push(...ticketChunk);
+        }
+
+        res.json({ success: true, tickets });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Failed to send push notifications' });
+    }
+});
+
+
+
+
+app.get('/send-notification/:token', async (req, res) => {
+
+    const token = req.params.token;
+    
+    const message = [{
+        to: token,
+        sound: 'default',
+        title: "hello test",
+        body: "this is message from quaid ",
+    }];
+
+    try {
+        // Send the push notifications
+        const chunks = expo.chunkPushNotifications(message);
+        const tickets = [];
+        for (const chunk of chunks) {
+            const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            tickets.push(...ticketChunk);
+        }
+
+        res.json({ success: true, tickets });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Failed to send push notifications' });
+    }
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
